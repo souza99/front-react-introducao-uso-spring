8ee371191a76635171a7398b8bbbb0497d977389
souza99 <jpfsouza99@gmail.com>
2022-11-20 18:16:26 -0300

    Corrigindo pagina login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,27 +27,27 @@ const mostrarMensagemErro = (erro) =>{
         <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
             <div className="text-center mb-5">
                 <img src="images/blocks/logos/hyper.svg" alt="hyper" height="50" className="mb-3" />
-                <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                <button className="p-link font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</button>
+                <div className="text-900 text-3xl font-medium mb-3">Bem vindo!</div>
+                <span className="text-600 font-medium line-height-3">Ainda não tem conta?</span>
+                <button className="p-link font-medium no-underline ml-2 text-blue-500 cursor-pointer">Cadastrar!</button>
             </div>
 
             <div>
                 <label htmlFor="email1" className="block text-900 font-medium mb-2">Email</label>
                 <InputText id="email1" type="text" className="w-full mb-3" onChange={(e)=>setEmail(e.target.value)}/>
 
-                <label htmlFor="password1" className="block text-900 font-medium mb-2">Password</label>
+                <label htmlFor="password1" className="block text-900 font-medium mb-2">Senha</label>
                 <InputText id="password1" type="password" className="w-full mb-3" onChange={(e)=>setSenha(e.target.value)}/>
 
                 <div className="flex align-items-center justify-content-between mb-6">
                     <div className="flex align-items-center">
                         <Checkbox inputId="rememberme1" binary className="mr-2"  />
-                        <label htmlFor="rememberme1">Remember me</label>
+                        <label htmlFor="rememberme1">Ficar conectado</label>
                     </div>
-                    <button className="p-link font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</button>
+                    <button className="p-link font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Esqueceu a senha?</button>
                 </div>
 
-                <Button onClick={()=>fazerLogin()} label="Sign In" icon="pi pi-user" className="w-full" />
+                <Button onClick={()=>fazerLogin()} label="Entrar" icon="pi pi-user" className="w-full" />
             </div>
         </div>
     </div>
